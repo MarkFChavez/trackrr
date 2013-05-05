47c987d514a0cbb5221658deb6dced6c3fb3db44
= <los_populares@yahoo.com>
2013-05-05 08:41:49 -0700

    Fixed controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,10 +4,17 @@ class HomeController < ApplicationController
   respond_to :html, :json, :xml
 
   def index
-  	@page_title = current_user.hashtag.name || "#trackrr"
-  	@tag = current_user.hashtag
-  	@tweets = Twitter.search(@page_title, :lang => 'en', :count => 30).results
-  	respond_with(@tweets)
+  	if !current_user.hashtag.nil?
+	  	@page_title = current_user.hashtag.name || "#trackrr"
+	  	@tag = current_user.hashtag
+	  	@tweets = Twitter.search(@page_title, :lang => 'en', :count => 30).results
+	  	respond_with(@tweets)
+	else
+		@page_title = "#trackrr"
+	  	@tag = current_user.hashtag
+	  	@tweets = Twitter.search(@page_title, :lang => 'en', :count => 30).results
+	  	respond_with(@tweets)
+	end
   end
 
 end
